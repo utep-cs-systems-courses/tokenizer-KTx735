6043e3dc5ec26239acfa6a775cbfd5f24b194a10
KTx735 <mygame1980@hotmail.com>
2021-09-14 13:17:28 -0600

    Added first two definitions

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -5,12 +5,27 @@
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c)
+{
+  if(c == ' ' || c == '\t' || c == '\0')
+    {
+      return 1;
+  }
+  return 0;
+}
 
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+int non_space_char(char c)
+{
+  if(c == '\t ' || c == '\n')
+    {
+      return 1;
+    }
+  return 0;
+}
+
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
