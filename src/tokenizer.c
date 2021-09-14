b57ed5ea787b6d3a0a0e3e169904db3fdc2a4848
KTx735 <mygame1980@hotmail.com>
2021-09-14 16:06:17 -0600

    Added definition for word_start and word_terminator

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -18,3 +18,20 @@ int non_space_char(char c)
   }
   return 0;
 }
+
+int *word_start(char *str)
+{
+  int start = 0;
+  while(space_char(str[start]))
+    start++;
+  return &str[start];
+}
+
+int *word_terminator(char *word)
+{
+  word = word_start(word);
+  int end = 0;
+  while(space_char(word[end]))
+    end++;
+  return &word[end];
+}
