c9c7e066c748e55706366487436d25fe9e6cb37a
KTx735 <mygame1980@hotmail.com>
2021-09-14 15:38:12 -0600

    Reomved definitions placed in the in tokenizer.h instead of tokenizer.c

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -6,26 +6,11 @@
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
 int space_char(char c)
-{
-  if(c == ' ' || c == '\t' || c == '\0')
-    {
-      return 1;
-  }
-  return 0;
-}
 
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c)
-{
-  if(c == '\t ' || c == '\n')
-    {
-      return 1;
-    }
-  return 0;
-}
-
+int non_space_char(char c);
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
