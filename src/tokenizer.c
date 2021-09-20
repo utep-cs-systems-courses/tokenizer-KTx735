28e76e6021f582f1535c8a84f02d6e7f4a0f4cf7
KTx735 <mygame1980@hotmail.com>
2021-09-20 11:29:25 -0600

    Added methods for the tokens

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -4,7 +4,7 @@
 
 int spar_char(char c)
 {
-  if(c == '\t' || c == ' '){
+  if (c == '\t' || c == ' '){
     return 1;
   }
   return 0;
@@ -12,7 +12,7 @@ int spar_char(char c)
 
 int non_space_char(char c)
 {
-  if(c != '\t' && c != ' ' && c != '\0')
+  if (c != '\t' && c != ' ' && c != '\0')
     {
       return 1;
   }
@@ -22,7 +22,7 @@ int non_space_char(char c)
 int *word_start(char *str)
 {
   int start = 0;
-  while(space_char(str[start]))
+  while (space_char(str[start]))
     start++;
   return &str[start];
 }
@@ -31,7 +31,7 @@ int *word_terminator(char *word)
 {
   word = word_start(word);
   int end = 0;
-  while(space_char(word[end]))
+  while (space_char(word[end]))
     end++;
   return &word[end];
 }
@@ -49,6 +49,7 @@ int count_words(char *str)
       return counter;
 }
 
+/* Allocate the requested memory and returns a pointer */
 int *copy_str(char *inStr, short len)
 {
   char *str = (char *) malloc((len + 1) * sizeof (char)); 
@@ -59,4 +60,47 @@ int *copy_str(char *inStr, short len)
     }
   str[len] = '\0';
   return str;
+}
+
+char **tokenize(char* str)
+{
+  int numberOfWords = count_words(str);
+  char **words = malloc((numberOfWords + 1) * sizeof(char));
+
+  for (int i = 0; i < numberOfWords; i++)
+    {
+      char *starOftWord = word_start(str);
+      char *endOfWord = word_terminator(str);
+      int lengthOfWord = endOfWord - startOfWorld;
+      words[i] = copy_str(startOfWord, lengthOfWord);
+      str = endOfWord;
+    }
+  
+/* Sets the last string to null */ 
+ words[numberOfWords] = NULL;
+ return words;
+}
+
+void print_tokens(char **tokens)
+{
+  int token = 0;
+  printf("\n");
+  while (token[token] != NULL)
+    {
+      printf("%d) %s");
+      printf("\n", token+1, tokens[token]);
+      token++;
+    }
+}
+
+/* free() deallocates the memory */ 
+void free_tokens(char **tokens)
+{
+  int i = 0;
+  while (tokens[i] != NULL)
+    {
+      free(tokens[i]);
+      i++;
+    }
+  free(tokens;)
 }
